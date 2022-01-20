7d44dc545839f7b2481aea202535e082152af3da
nicecode22 <nicecode22@gmail.com>
2022-01-20 13:56:21 -0600

    commit

--- models/product.js
+++ models/product.js
@@ -1,39 +1,46 @@
+const path = require('path');
 const fs = require('fs');
 
-const fetch = require('node-fetch');
-const productsUrl = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+const p = path.join(__dirname, '..', 'data', 'products.json');
 
 module.exports = class Product {
     static fetchAll(cb) {
-        fetch(productsUrl)
-        .then((res) => res.json())
-        .then((products) => {
+        fs.readFile(p, (err, fileContent) => {
+            let products;
+            if (err) {
+                products = [];
+            } else {
+                products = JSON.parse(fileContent);
+            }
             cb(products);
-        })
-    .catch((err) => console.log(err));
-}
+        });
+    }
 
-static search(query, cb) {
-    fetch(productsUrl)
-    .then((res) => res.json())
-    .then((products) => {
+    static search(query, cb) {
+        fs.readFile(p, (err, fileContent) => {
+            let products;
+            if (err)
+                products = [];
+            else
+                products = JSON.parse(fileContent);
+
             // search products
-            const filteredProducts = products.filter((product) => {
-            // search product tags
-            let tagFound = false;
-            product.tags.forEach((tag) => {
-                if (tag.toLowerCase().includes(query)) tagFound = true;
-            });
+            const filteredProducts = products.filter(product => {
+                // search product tags
+                let tagFound = false;
+                product.tags.forEach(tag => {
+                    if (tag.toLowerCase().includes(query))
+                        tagFound = true;
+                });
 
-            return (
-                tagFound ||
-                product.name.toLowerCase().includes(query) ||
-                product.description.toLowerCase().includes(query)
-            );
+                return tagFound ||
+                    product.name.toLowerCase().includes(query) ||
+                    product.description.toLowerCase().includes(query);
             });
 
             cb(filteredProducts);
-        })
-    .catch((err) => console.log(err));
+
+        });
     }
 };
+
