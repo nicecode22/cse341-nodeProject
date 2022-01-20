87e64a18576283c7513979d279dbd1ccb0a5d3bc
nicecode22 <nicecode22@gmail.com>
2022-01-20 12:38:20 -0600

    commit

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,6 +3,24 @@
 const express = require('express');
 const router = express.Router();
 
+const newUserArray = ['Pepe', 'Lorenzo', 'Camila'];
+
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+  newUserArray.push(newUser);
+  res.redirect('/ta02');
+});
+
+router.post('/removeUSer', (req, res, next) => {
+  const removeUser = req.body.removeUser;
+  const removed = newUserArray.indexOf(removeUser);
+  if (removed !== -1) {
+    newUserArray.splice(removed, 1);
+  } else {
+    return
+  }
+  res.redirect('/ta02');
+})
 router.get('/', (req, res, next) => {
   res.render('pages', {
     title: 'Team Activity 02',
@@ -11,5 +29,7 @@ router.get('/', (req, res, next) => {
     contentCSS: true, // For HBS
   });
 });
+
+
 
 module.exports = router;
